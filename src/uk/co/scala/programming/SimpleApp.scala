6a2556d5a729638dc2398def029cea0c3df060bf
Clelio De Souza <cleliofs@gmail.com>
2013-07-07 17:54:06 +0100

    Adding more small scala samples

--- src/uk/co/scala/programming/SimpleApp.scala
+++ src/uk/co/scala/programming/SimpleApp.scala
@@ -22,6 +22,11 @@ object Hello extends App {
   capitals.values.foreach(v => println(v.toLowerCase()));
   
   brutalIterator(capitals.keys)
+  brutalIterator(capitals.keys.toArray)
+
+  def brutalIterator(list: Array[String]) = {
+	  list.foreach(i => print(i))
+  }
   
   def brutalIterator(list: Iterable[String]) = {
     for (i <- list) {
@@ -32,4 +37,24 @@ object Hello extends App {
   val MAX = general.max(10, 5)
   println(MAX)
   println(general.max(10, 15))
+  
+  val greetings = new Array[String](3)
+  greetings(0) = "Hello"
+  greetings(1) = ", "
+  greetings(2) = "world!"
+  for (i <- 0 to (greetings.length - 1)) print(greetings(i))
+  greetings.foreach(i => print(i))
+  
+  val oneTwo = List(1, 2)
+  val threeFour = List(3, 4)
+  val oneTwoThreeFour = oneTwo ::: threeFour
+  println(oneTwoThreeFour)
+  
+  val oneTwoThree = 1 :: 2 :: 3 :: Nil
+  println(oneTwoThree)
+  if (oneTwoThree.exists(i => i == 1)) print("Number 1 exists")
+  if (oneTwoThree.exists(i => i == 4)) print("Number 4 exists")
+  val newOneTwoThree = 4 :: oneTwoThree
+  if (newOneTwoThree.exists(i => i == 4)) print("Now number 4 exists on new list")
+    
 }
